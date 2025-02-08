88154162acec7035dfe8d7d7ed0deac27030f540
Fabio Peluso <fabio.peluso@grupposcai.it>
2025-02-08 17:46:53 +0100

    first commit

---
+++ src/main/java/it/peluso/balanceauth/BalanceAuthApplication.java
@@ -0,0 +1,13 @@
+package it.peluso.balanceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceAuthApplication.class, args);
+    }
+
+}
---
+++ src/main/java/it/peluso/balanceauth/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package it.peluso.balanceauth.service;
+
+import it.peluso.balanceauth.entity.User;
+import it.peluso.balanceauth.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .collect(Collectors.toSet()))
+                .disabled(!user.isEnabled())
+                .build();
+    }
+}
